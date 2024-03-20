190d05b3974ca43f2b9429c8cf95746b8f42420c
Raphael Barros <antitribus@gmail.com>
2024-03-20 09:02:44 -0300

    Create git-alias.sh

---
+++ git-alias.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Configuração de alias de comandos git no linux
+
+echo "
+-------------------------------------------------------------------
+name: alias-git
+description: Configuração de alias de comandos git no linux
+author: Raphael Barros <antitribus@gmail.com.br>
+-------------------------------------------------------------------";
+echo ""
+
+echo "> Lista de alias adicionados --------------------------------------"
+
+echo '
+gb="git branch"
+gbd="git branch --delete"
+gck="git checkout"
+gckb="git checkout -b"
+gcl="git clone"
+gcm="git commit"
+gcma="git commit --amend"
+gft="git fetch"
+gfta="git fetch --all"
+glg="git log"
+glgo="git log --oneline"
+gpl="git pull"
+gps="git push"
+grb="git rebase"
+grba="git rebase --abort"
+grba="git rebase --continue"
+grba="git rebase --abort"
+grbc="git rebase --continue"
+gst="git status"
+gsh="git stash"
+gshp="git stash pop"';
+
+alias gb="git branch"
+alias gbd="git branch --delete"
+alias gck="git checkout"
+alias gckb="git checkout -b"
+alias gcl="git clone"
+alias gcm="git commit"
+alias gcma="git commit --amend"
+alias gft="git fetch"
+alias gfta="git fetch --all"
+alias glg="git log"
+alias glgo="git log --oneline"
+alias gpl="git pull"
+alias gps="git push"
+alias grb="git rebase"
+alias grba="git rebase --abort"
+alias grba="git rebase --continue"
+alias grba="git rebase --abort"
+alias grbc="git rebase --continue"
+alias gst="git status"
+alias gsh="git stash"
+alias gshp="git stash pop"
+
+echo "> Fim da configuração dos alias -----------------------------------";
+echo ""
